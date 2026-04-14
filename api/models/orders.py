a730724c55092729bd7303e23685e83c801c39af
ryansawin <rsawin@charlotte.edu>
2026-04-13 21:01:03 -0400

    feat: minor edits made to orders, order_details, sandwiches

--- api/models/orders.py
+++ api/models/orders.py
@@ -7,9 +7,10 @@ from ..dependencies.database import Base
 class Order(Base):
     __tablename__ = "orders"
 
-    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True, nullable=False)
     customer_name = Column(String(100))
-    order_date = Column(DATETIME, nullable=False, server_default=str(datetime.now()))
+    order_date = Column(DATETIME, nullable=False, default=datetime.now)
     description = Column(String(300))
 
+    customer = relationship("Customer", back_populates="orders")
     order_details = relationship("OrderDetail", back_populates="order")
