c244ec5d2fa871d391776d6c4d82cc86b7831c87
ryansawin <rsawin@charlotte.edu>
2026-04-26 21:06:09 -0400

    feat: recipes and sandwiches controllers and routers

---
+++ api/controllers/sandwiches.py
@@ -0,0 +1,70 @@
+from fastapi import HTTPException, Response, status
+from sqlalchemy.orm import Session
+from sqlalchemy.exc import SQLAlchemyError
+from ..models import sandwiches as model
+from ..schemas import sandwiches as schema
+
+
+def create(db: Session, request: schema.SandwichCreate):
+    try:
+        new_item = model.Sandwich(
+            sandwich_name=request.sandwich_name,
+            price=request.price
+        )
+        db.add(new_item)
+        db.commit()
+        db.refresh(new_item)
+    except SQLAlchemyError as e:
+        db.rollback()
+        error = str(e.__dict__.get('orig', e))
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=error)
+    return new_item
+
+
+def read_all(db: Session):
+    try:
+        result = db.query(model.Sandwich).all()
+    except SQLAlchemyError as e:
+        error = str(e.__dict__.get('orig', e))
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=error)
+    return result
+
+
+def read_one(db: Session, item_id: int):
+    try:
+        item = db.query(model.Sandwich).filter(model.Sandwich.id == item_id).first()
+        if not item:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Id not found!")
+    except SQLAlchemyError as e:
+        error = str(e.__dict__.get('orig', e))
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=error)
+    return item
+
+
+def update(db: Session, item_id: int, request: schema.SandwichUpdate):
+    try:
+        item = db.query(model.Sandwich).filter(model.Sandwich.id == item_id)
+        if not item.first():
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Id not found!")
+        update_data = request.model_dump(exclude_unset=True)
+        item.update(update_data, synchronize_session=False)
+        db.commit()
+    except SQLAlchemyError as e:
+        db.rollback()
+        error = str(e.__dict__.get('orig', e))
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=error)
+    return item.first()
+
+
+def delete(db: Session, item_id: int):
+    try:
+        item = db.query(model.Sandwich).filter(model.Sandwich.id == item_id)
+        if not item.first():
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Id not found!")
+        item.delete(synchronize_session=False)
+        db.commit()
+    except SQLAlchemyError as e:
+        db.rollback()
+        error = str(e.__dict__.get('orig', e))
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=error)
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
--- api/routers/index.py
+++ api/routers/index.py
@@ -1,6 +1,8 @@
-from . import orders, order_details
+from . import orders, order_details, recipes, sandwiches
 
 
 def load_routes(app):
     app.include_router(orders.router)
     app.include_router(order_details.router)
+    app.include_router(recipes.router)
+    app.include_router(sandwiches.router)
