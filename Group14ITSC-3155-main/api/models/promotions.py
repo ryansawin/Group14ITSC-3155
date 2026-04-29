2c0d5488f61e086cd21ebc67c3e18c2e7c6dd444
Cameron <cjone399@charlotte.edu>
2026-04-29 01:58:32 -0400

    temp changes

--- Group14ITSC-3155-main/api/models/promotions.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy import Column, Integer, String, Date
-from ..dependencies.database import Base
-
-class Promotion(Base):
-    __tablename__ = "promotions"
-    id = Column(Integer, primary_key=True)
-    code = Column(String(50), nullable=False)
-    expiry_date = Column(Date, nullable=False)
