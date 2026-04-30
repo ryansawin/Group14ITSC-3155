c777b786417c7aa0dff794d0a227453e5224fb8b
Champt10 <christian.leo.hampton@gmail.com>
2026-04-30 19:22:33 -0400

    Resources updated

--- api/routers/index.py
+++ api/routers/index.py
@@ -1,6 +1,7 @@
-from . import orders, order_details
+from . import orders, order_details, resources
 
 
 def load_routes(app):
     app.include_router(orders.router)
     app.include_router(order_details.router)
+    app.include_router(resources.router)
